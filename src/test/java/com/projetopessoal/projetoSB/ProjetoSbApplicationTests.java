6cb8cf750216818553a62bc8df228a473e3e582e
Victor Gabriel <victo091198@gmail.com>
2022-10-27 11:13:31 -0300

    Criando Projeto

---
+++ src/test/java/com/projetopessoal/projetoSB/ProjetoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetopessoal.projetoSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
