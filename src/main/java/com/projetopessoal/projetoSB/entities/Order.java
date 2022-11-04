e6a74166ed08f2d5276e63fa7b365844965a451b
Victor Gabriel <victo091198@gmail.com>
2022-11-04 17:09:35 -0300

    Metodos Subtotal e Total criados

--- src/main/java/com/projetopessoal/projetoSB/entities/Order.java
+++ src/main/java/com/projetopessoal/projetoSB/entities/Order.java
@@ -91,6 +91,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        double total = 0.0;
+        for (OrderItem orderItem : items) {
+            total += orderItem.getSubTotal();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
